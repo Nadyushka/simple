ca5049273ef3aec087f424a7d6128a424a893af3
Nadyushka <12nadya11@gmail.com>
2024-09-27 09:22:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/**
+ * Логика для header
+ */
+
+const headerSearchBtn = document.querySelector('.header__search-btn')
+const headerSearchInput = document.querySelector('.header__input-search')
+const headerSearchInputCloseButton = document.querySelector('.header__search-btn-close')
+
+const toggleHeaderSearchInput = (isOpen) => {
+    if (isOpen) {
+        headerSearchInput.classList.remove('header__input-search_visible')
+        headerSearchInputCloseButton.style.display = 'none'
+        headerSearchInput.value = ''
+    } else {
+        headerSearchInput.classList.add('header__input-search_visible')
+        headerSearchInputCloseButton.style.display = 'flex'
+    }
+}
+
+headerSearchBtn.addEventListener('click', () => toggleHeaderSearchInput(false))
+headerSearchInputCloseButton.addEventListener('click', ()=>toggleHeaderSearchInput(true))
