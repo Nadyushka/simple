2ff5f4206316feaea4300dac1e2e7805ad0f615c
Nadyushka <12nadya11@gmail.com>
2024-09-30 09:42:46 +0300

    change dialog class naming

--- script.js
+++ script.js
@@ -19,3 +19,4 @@ const toggleHeaderSearchInput = (isOpen) => {
 
 headerSearchBtn.addEventListener('click', () => toggleHeaderSearchInput(false))
 headerSearchInputCloseButton.addEventListener('click', ()=>toggleHeaderSearchInput(true))
+
